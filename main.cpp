3dd99f34c3932e9dcde68072586cb948ca2cf681
hulc <3314986382@qq.com>
2023-10-09 23:10:40 +0800

    VER2 个位数运算DEBUG版‘

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <stack>
-#include <type_traits>
 
 using namespace std ;
 
@@ -11,7 +10,7 @@ bool isNum(char word){
 }
 
 bool isAsign(char word){
-  return word==43||word==45||word==42||word==47;
+  return word==43||word==45||word==42||word==47 ; // ||word==61;
 }
 
 bool isLeftKuohao(char word){
@@ -30,7 +29,7 @@ short getRank(char word){
   if(isLeftKuohao(word)) { return 0 ;}
   if(word == 43||word==45) { return 1;}
   if(word==42||word==47) { return 2 ;}
-  return 114;
+  return 1;
 }
 
 int calc ( char sign , int num1, int num2){
@@ -44,7 +43,7 @@ int calc ( char sign , int num1, int num2){
 }
 
 void computeLast(stack<int> * mStack, stack<char> * sStack){
-        if (isLeftKuohao( sStack-> top())){return;}
+        if (isLeftKuohao( sStack-> top())||sStack->empty()){return;}
         int tempNum1 = mStack -> top();mStack->pop();
         int tempNum2 = mStack -> top();mStack-> pop();
         mStack->push(calc(sStack->top(),tempNum1 , tempNum2));
@@ -63,35 +62,42 @@ int  getAnswer ( ) {
   char thisWord;
   bool flag = true;
   for (int ii =0 ; flag != false &&( thisWord=s[ii])!=13 ; ii +=1){//和回车
-  
-
+    cout<< ii+1<<" Loop  " << thisWord<<endl; 
+    if(isEq(thisWord)){flag = false;
+      for (;!sStack->empty();)   //死循环！！！！！！
+      {computeLast(mStack,sStack);}
+    }
+   
     if (isNum(thisWord)){mStack->push(thisWord-48);}
     if(isLeftKuohao(thisWord)){sStack ->push(thisWord);}
     if( isAsign(thisWord)){
       if ( sStack->empty() || getRank(thisWord)>getRank(sStack-> top()) ){
+        cout<<"isAsign >"<<endl;
         sStack->push(thisWord);
       }
-      else if (getRank(thisWord)==sStack->top()){
+      else if (getRank(thisWord)==getRank(sStack->top())){
+        cout << "isAsign =" << endl;
         computeLast(mStack,sStack);
         sStack->push(thisWord);
       }
-      else if (getRank(thisWord)<sStack->top()){
-
+      else if (getRank(thisWord)<getRank(sStack->top())){
+        cout << "isAsign <" << endl;
         computeLast(mStack,sStack);
         sStack->push(thisWord);
+        
       }
     }
+ 
     if ( isRightKuohao(thisWord) ) {
-
-      computeLast( mStack, sStack);
+      for(;!isLeftKuohao(sStack->top());){
+      computeLast( mStack, sStack);}
       sStack->pop();//希望sStack->top();是左括号
       
-    }
-    if(isEq(thisWord)){flag = false;
-      computeLast(mStack,sStack);
+      
     }
 
-    cout<< ii+1<<" Loop"<<endl;
+
+    cout << "mTop: "<< mStack->top()<<endl; 
     
   }
 
@@ -99,11 +105,14 @@ int  getAnswer ( ) {
   mStack->pop();
 
   if(mStack -> size() > 0||!sStack->empty()){
+    cout<<mStack->top()<<endl;
+    cout<<sStack->top()<<endl;
     delete sStack;delete mStack;
     return 114514;
   }
   else {
     delete sStack ; delete mStack;
+    cout << "anser is :"<< endl;
     return ans ; 
   }
 }
